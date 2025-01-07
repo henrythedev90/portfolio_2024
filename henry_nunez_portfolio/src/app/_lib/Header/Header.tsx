325efae2e8e743ef794e89e2d64d067aa7d6be9b
henrythedev90 <henrythedev90@gmail.com>
2025-01-07 17:34:47 -0500

    starting to set up a hamburger in the menu

--- henry_nunez_portfolio/src/app/_lib/Header/Header.tsx
+++ henry_nunez_portfolio/src/app/_lib/Header/Header.tsx
@@ -1,12 +1,43 @@
-import React from "react";
+"use client";
+import React, { useEffect, useRef } from "react";
 import Container from "../../../../Components/Container/Container";
 import classes from "./Header.module.css";
 import Link from "next/link";
 import { NAV_LINK } from "../../../../Components/data/navLinks";
 
 const Header = () => {
+  {
+    /**useRef is null at start, you need to check node's existence before using it.*/
+  }
+  const headerRef = useRef<HTMLElement>(null);
+  const menuRef = useRef<HTMLElement>(null);
+
+  const headerFunc = () => {
+    if (headerRef.current) {
+      if (
+        document.body.scrollTop > 5 ||
+        document.documentElement.scrollTop > 5
+      ) {
+        headerRef.current.classList.add(classes.header_shrink);
+      } else {
+        headerRef.current.classList.remove(classes.header_shrink);
+      }
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", headerFunc);
+    return () => window.removeEventListener("scroll", headerFunc);
+  }, []);
+
+  const toggleMenu = () => {
+    if (menuRef.current) {
+      menuRef.current.classList.toggle(`${classes.menu_active}`);
+    }
+  };
+
   return (
-    <header className={classes.header}>
+    <header className={classes.header} ref={headerFunc}>
       <Container>
         <div className={classes.nav_wrapper}>
           {/** potential logo will go here */}
@@ -34,8 +65,8 @@ const Header = () => {
               </div>
             </div>
           </div>
-          <span>
-            <i className="ri-menu-line"></i>
+          <span className={classes.mobile_menu}>
+            <i className={classes.menu_line} onClick={toggleMenu}></i>
           </span>
         </div>
       </Container>
