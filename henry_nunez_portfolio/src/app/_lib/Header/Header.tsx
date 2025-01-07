a29fd33381d73d8000fbc55d5508fada41abebdd
henrythedev90 <henrythedev90@gmail.com>
2025-01-07 17:49:50 -0500

    still working on the navigation

--- henry_nunez_portfolio/src/app/_lib/Header/Header.tsx
+++ henry_nunez_portfolio/src/app/_lib/Header/Header.tsx
@@ -10,7 +10,7 @@ const Header = () => {
     /**useRef is null at start, you need to check node's existence before using it.*/
   }
   const headerRef = useRef<HTMLElement>(null);
-  const menuRef = useRef<HTMLElement>(null);
+  const menuRef = useRef<HTMLDivElement>(null);
 
   const headerFunc = () => {
     if (headerRef.current) {
@@ -49,7 +49,11 @@ const Header = () => {
             </Link>
           </div>
           {/** the navigation menu will go here */}
-          <div className={classes.navigation}>
+          <div
+            className={`${classes.navigation}`}
+            onClick={toggleMenu}
+            ref={menuRef}
+          >
             <div className={classes.nav_menu}>
               {NAV_LINK.map((item, index) => (
                 <Link href={item.path} key={index}>
@@ -66,7 +70,9 @@ const Header = () => {
             </div>
           </div>
           <span className={classes.mobile_menu}>
-            <i className={classes.menu_line} onClick={toggleMenu}></i>
+            <div className={classes.menu_line} onClick={toggleMenu}>
+              <div>{/** this is the hamburger in the mobile menu*/}</div>
+            </div>
           </span>
         </div>
       </Container>
