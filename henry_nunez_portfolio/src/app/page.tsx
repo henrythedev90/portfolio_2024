860258cd4bd7653c5e77dc6e9ba15af6cc707bb3
henrythedev90 <henrythedev90@gmail.com>
2025-01-07 13:57:13 -0500

    first implementations

---
+++ henry_nunez_portfolio/src/app/page.tsx
@@ -0,0 +1,15 @@
+import { Fragment } from "react";
+// import Image from "next/image";
+// import styles from "./page.module.css";
+import Layout from "../../components/Layout/Layout";
+
+export default function Home() {
+  return (
+    <Fragment>
+      <div>Hero</div>
+      <div>Skills</div>
+      <div>Project</div>
+      <div>Contact</div>
+    </Fragment>
+  );
+}
