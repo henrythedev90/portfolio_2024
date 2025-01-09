e7af3d7e9fc9b6a8f82d9082ce76d02438b557e6
henrythedev90 <henrythedev90@gmail.com>
2025-01-09 18:16:07 -0500

    skills on mobile

--- henry_nunez_portfolio/Components/data/skillsList.ts
+++ henry_nunez_portfolio/Components/data/skillsList.ts
@@ -95,3 +95,10 @@ export const SKILLS: { icon: string; title: string; type: string }[] = [
     type: "Back_End",
   },
 ];
+
+export const SKILLS_TYPE: { path: string; type: string }[] = [
+  { path: "/Front-end", type: "Front_End" },
+  { path: "/Back-end", type: "Back_End" },
+  { path: "/Tools", type: "Tools" },
+  { path: "/Testing", type: "Testing" },
+];
--- henry_nunez_portfolio/src/app/_lib/Skills/Skills.tsx
+++ henry_nunez_portfolio/src/app/_lib/Skills/Skills.tsx
@@ -1,14 +1,16 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Container from "../../../../Components/Container/Container";
 import classes from "./Skills.module.css";
 import SkillsImage from "../../../../Components/SkillsImage/SkillsImage";
-import { SKILLS } from "../../../../Components/data/skillsList";
+import { SKILLS, SKILLS_TYPE } from "../../../../Components/data/skillsList";
 
 const Skills = () => {
+  const [openSkills, setOpenSkills] = useState<string | null>(null);
   return (
     <div className={classes.skills_section}>
       <Container>
-        <div className={classes.skills_container}>
+        {/* <div className={classes.skills_container}>
           <div className={classes.skill_row_one}>
             {SKILLS.slice(0, 3).map(
               (item: { icon: string; title: string }, index: number) => (
@@ -60,6 +62,52 @@ const Skills = () => {
               <SkillsImage key={index} icon={item.icon} title={item.title} />
             ))}
           </div>
+        </div> */}
+        <div className={classes.skills_list_mobile}>
+          {SKILLS_TYPE.map((skillType) => (
+            <div
+              key={skillType.type}
+              className={`skills_${skillType.type
+                .toLowerCase()
+                .replace(" ", "_")}`}
+            >
+              <div
+                className={classes.toggle_skills}
+                onClick={(e) => {
+                  e.preventDefault();
+                  setOpenSkills(
+                    openSkills === skillType.type ? null : skillType.type
+                  );
+                }}
+              >
+                <h1 className={classes.skills_list_mobile_title}>
+                  {skillType.type.replace("_", "-")}
+                </h1>
+                {openSkills === skillType.type ? (
+                  <div className={classes.chevron_arrow_up}></div>
+                ) : (
+                  <div className={classes.chevron_arrow_down}></div>
+                )}
+              </div>
+              {openSkills === skillType.type && (
+                <ul
+                  className={`${classes.skills_list_mobile_item}${
+                    openSkills === skillType.type ? "open" : ""
+                  }`}
+                >
+                  {SKILLS.filter((item) => item.type === skillType.type).map(
+                    (item, index) => (
+                      <SkillsImage
+                        key={index}
+                        title={item.title}
+                        icon={item.icon}
+                      />
+                    )
+                  )}
+                </ul>
+              )}
+            </div>
+          ))}
         </div>
       </Container>
     </div>
