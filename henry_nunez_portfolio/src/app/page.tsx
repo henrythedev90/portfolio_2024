8aae27f8cdfb00752ee0e8f677c91c5ee3787f21
henrythedev90 <henrythedev90@gmail.com>
2025-01-24 13:22:21 -0500

    fixed the header and footer

--- henry_nunez_portfolio/src/app/page.tsx
+++ henry_nunez_portfolio/src/app/page.tsx
@@ -1,42 +1,16 @@
-"use client";
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Hero from "./_lib/Hero/Hero";
 import Skills from "./_lib/Skills/Skills";
 import Contact from "./_lib/Contact/Contact";
-import Header from "./_lib/Header/Header";
-import Footer from "./_lib/Footer/Footer";
-import useLocalStorage from "use-local-storage";
+import RootLayout from "./layout";
 
 export default function Home() {
-  const [theme, setTheme] = useLocalStorage("theme", "light");
-  const [isMounted, setIsMounted] = useState(false);
-
-  const availableThemes = ["light", "dark", "blue", "green"];
-
-  useEffect(() => {
-    const themeMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
-    if (!theme) {
-      setTheme(themeMode ? "dark" : "light");
-    }
-    setIsMounted(true);
-  }, [theme, setTheme]);
-
-  if (!isMounted) {
-    return null;
-  }
-
   return (
-    <body data-theme={theme}>
-      <Header />
+    <RootLayout>
       <Hero />
       <Skills />
       {/* <div>Project</div> */}
       <Contact />
-      <Footer
-        theme={theme}
-        setTheme={setTheme}
-        availableThemes={availableThemes}
-      />
-    </body>
+    </RootLayout>
   );
 }
