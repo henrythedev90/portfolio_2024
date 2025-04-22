a0bf7af721e8467d3f1fd343ee78856885c34aa2
henrythedev90 <henrythedev90@gmail.com>
2025-04-22 00:22:45 -0400

    almost done with the project section

--- henry_nunez_portfolio/src/app/_lib/ProjectCard/ProjectCard.tsx
+++ henry_nunez_portfolio/src/app/_lib/ProjectCard/ProjectCard.tsx
@@ -1,19 +1,40 @@
 import React from "react";
 import Link from "next/link";
-
-const ProjectCard = ({ project }: { project: any }) => {
+import Container from "@/app/components/Container/Container";
+import { Project } from "@/app/components/data/projects";
+import classes from "./style/ProjectCard.module.css";
+import Image from "next/image";
+const ProjectCard = ({ project }: { project: Project }) => {
   return (
-    <div>
-      <h3>{project.title}</h3>
-      <Link href={project.github} target="_blank">
-        GitHub
-      </Link>
-      <Link href={project.link} target="_blank">
-        Live
-      </Link>
-      <p>{project.description}</p>
-      <p>{project.technologies.join(", ")}</p>
-    </div>
+    <Container>
+      <div className={classes.project_card_container}>
+        <div className={classes.project_card}>
+          <h3>{project.title}</h3>
+          <Link href={project.github} target="_blank">
+            GitHub
+          </Link>
+          <Link href={project.link} target="_blank">
+            Live
+          </Link>
+        </div>
+        <div className={classes.project_card_technologies}>
+          {project.technologies.map((technology: string) => (
+            <p key={technology}>{technology}</p>
+          ))}
+        </div>
+        <div className={classes.project_card_description}>
+          <p>{project.description}</p>
+        </div>
+        <div className={classes.project_card_image}>
+          <Image
+            src={project.image}
+            alt={project.image_alt}
+            width={360}
+            height={280}
+          />
+        </div>
+      </div>
+    </Container>
   );
 };
 
--- henry_nunez_portfolio/src/app/components/data/projects.ts
+++ henry_nunez_portfolio/src/app/components/data/projects.ts
@@ -1,12 +1,23 @@
+export interface Project {
+  id: number;
+  title: string;
+  description: string;
+  image: string;
+  link: string;
+  github: string;
+  technologies: string[];
+  image_alt: string;
+}
 export const PROJECTS = [
   {
     id: 1,
     title: "Henry's Dog Adoption Agency",
     description:
       "A modern web application for browsing and finding your perfect canine companion! This app allows users to browse available dogs, filter by various criteria, add favorites, and receive personalized matches based on their preferences.",
-    image: "/images/projects/henrys-dog-adoption-agency.png",
     link: "https://henrys-dog-adoption-agency.vercel.app/",
-    github: "https://github.com/henry-nunez/henrys-dog-adoption-agency",
+    github: "https://github.com/henrythedev90/henrys-dog-adoption-agency",
+    image_alt: "Screenshot of Henry's Dog Adoption Agency",
+    image: "/images/henrys-dog-adoption-agency.vercel.app_dogs_app.png",
     technologies: [
       "Next.js",
       "React Redux",
@@ -21,9 +32,10 @@ export const PROJECTS = [
     title: "Weather App",
     description:
       "A modern, responsive weather application that displays real-time weather information and forecasts with dynamic themes that change based on current weather conditions.",
-    image: "/images/projects/weather-app.png",
+    image: "/images/weather-app-ten-psi-94.vercel.app.png",
     link: "https://weather-app-ten-psi-94.vercel.app/",
-    github: "https://github.com/henry-nunez/weather-app",
+    github: "https://github.com/henrythedev90/weather-app",
+    image_alt: "Screenshot of Weather App",
     technologies: [
       "Next.js",
       "TypeScript",
@@ -38,9 +50,10 @@ export const PROJECTS = [
     title: "Retro Digital Clock",
     description:
       "A retro-style digital clock that displays the current time in a classic, vintage-inspired design. The clock features a sleek, minimalistic look with a retro-futuristic vibe, perfect for adding a touch of nostalgia to any space.",
-    image: "/images/projects/retro-clock.png",
+    image: "/images/retro-clock.vercel.app.png",
     link: "https://retro-clock.vercel.app/",
     github: "https://github.com/henrythedev90/retro-clock",
+    image_alt: "Screenshot of Retro Digital Clock",
     technologies: [
       "Next.js",
       "TypeScript",
