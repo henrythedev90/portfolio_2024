e5ae1bd12389fd7d5050efde58bd99f520c05771
henrythedev90 <henrythedev90@gmail.com>
2025-04-22 11:16:17 -0400

    starting to create a blog section

---
+++ henry_nunez_portfolio/src/app/blog/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Container from "../components/Container/Container";
+
+export default function Blog() {
+  return (
+    <Container>
+      <div>This is the blog page</div>
+    </Container>
+  );
+}
--- henry_nunez_portfolio/src/app/components/data/navLinks.ts
+++ henry_nunez_portfolio/src/app/components/data/navLinks.ts
@@ -2,4 +2,5 @@ export const NAV_LINK = [
   { path: "/#skills", display: "Skills" },
   { path: "/#contact", display: "Contact" },
   { path: "/projects", display: "Projects" },
+  { path: "/blog", display: "Blog" },
 ];
