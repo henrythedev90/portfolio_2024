b29e24c340193c555a2323c094170b5eaa5c0f1b
henrythedev90 <henrythedev90@gmail.com>
2025-05-01 01:49:35 -0400

    no need to show off

--- henry_nunez_portfolio/src/app/page.tsx
+++ henry_nunez_portfolio/src/app/page.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import Hero from "./_lib/Hero/Hero";
 import Skills from "./_lib/Skills/Skills";
-import Carousel from "./components/Carousel/Carousel";
-import { PROJECTS } from "./components/data/projects";
+// import Carousel from "./components/Carousel/Carousel";
+// import { PROJECTS } from "./components/data/projects";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Skills />
-      <Carousel projects={PROJECTS} />
+      {/* <Carousel projects={PROJECTS} /> */}
     </>
   );
 }
