159c52afb5331185e0f8ed60bcaddd38f0b7cda4
henrythedev90 <henrythedev90@gmail.com>
2025-05-06 01:14:33 -0400

    vercel is handling it

--- henry_nunez_portfolio/next.config.js
+++ henry_nunez_portfolio/next.config.js
@@ -1,21 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async redirects() {
-    return [
-      // Only redirect non-www to www
-      {
-        source: "/:path*",
-        has: [
-          {
-            type: "host",
-            value: "henry-nunez.com",
-          },
-        ],
-        destination: "https://www.henry-nunez.com/:path*",
-        permanent: false,
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
