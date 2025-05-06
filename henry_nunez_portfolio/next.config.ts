e22680a20e227d2251b221a9a543f4857307634b
henrythedev90 <henrythedev90@gmail.com>
2025-05-06 00:31:45 -0400

    improving redirects

--- henry_nunez_portfolio/next.config.ts
+++ henry_nunez_portfolio/next.config.ts
@@ -1,7 +1,32 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "henry-nunez.com",
+          },
+        ],
+        destination: "https://www.henry-nunez.com/:path*",
+        permanent: true,
+      },
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "http://www.henry-nunez.com",
+          },
+        ],
+        destination: "https://www.henry-nunez.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
