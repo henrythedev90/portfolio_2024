aa2a02cf5c413bc5a5c036b8f8a2a3a80a10a9ec
henrythedev90 <henrythedev90@gmail.com>
2025-05-06 00:57:28 -0400

    fixed the redirects

--- henry_nunez_portfolio/next.config.js
+++ henry_nunez_portfolio/next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   async redirects() {
     return [
-      // Redirect non-www to www (both HTTP and HTTPS)
+      // Only redirect non-www to www
       {
         source: "/:path*",
         has: [
@@ -13,18 +13,6 @@ const nextConfig = {
         ],
         destination: "https://www.henry-nunez.com/:path*",
         permanent: true,
-      },
-      // Redirect HTTP www to HTTPS www
-      {
-        source: "/:path*",
-        has: [
-          {
-            type: "host",
-            value: "www.henry-nunez.com",
-          },
-        ],
-        permanent: true,
-        destination: "https://www.henry-nunez.com/:path*",
       },
     ];
   },
